69afc579eed6b2de2d88acfdc9193759cc3d0988
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-04 18:45:49 -0700

    Adds Task One

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - prints whether the number stored in n is positive or negative..
  * n: stores a different value every time the code is run.
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the last digit of the number stored in
+ * the variable n.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	int L;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
+	printf("Last digit of %d is %d", n, L);
+
+	if (L > 5)
+		printf("and is greater than 5\n");
+	else if (L == 0)
+		printf("and is 0\n");
+	else if (L < 6 && L != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}

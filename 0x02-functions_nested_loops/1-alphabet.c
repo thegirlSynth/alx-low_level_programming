73782cf9b6110ffd24b578fa7b768c399094c590
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-07 16:41:47 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n')
+		;
+	return (0);
+}

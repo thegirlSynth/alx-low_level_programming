1193fa06991be67a87d65e4df830b2fdfbfec65a
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-13 19:44:40 -0700

    Adds Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: String to be reversed.
+ *
+ */
+
+void rev_string(char *s)
+{
+	char store;
+	int count, red, num;
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	red = count - 1;
+
+	num = 0;
+	while (num < (count/2))
+	{
+		store = s[num];
+		s[num] = s[red];
+		s[red--] = store;
+
+		num++;
+	}
+}

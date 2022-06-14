cb5e08933aeb550a77b109b7cb19ceb69853efd1
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-13 18:09:19 -0700

    Adds Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout.
+ *
+ * @str: string to be printed.
+ */
+
+void _puts(char *str)
+{
+	int count;
+
+	count = 0;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count])
+	}
+	_putchar('\n');
+}

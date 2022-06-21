d76c5848b20343622d11937d36ea2053df98a374
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-21 16:28:21 -0700

    Adds Script 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,6 +17,9 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
-	  dest[i] = '\0';
-	  return (dest);
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }

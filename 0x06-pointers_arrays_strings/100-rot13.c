060bb5f5607f035513245ec7be895e0ab924f299
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-22 12:16:04 -0700

    Adds Script 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: the string to be encoded
+ *
+ * Return: the resulting string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char xter[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char val[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (s[i] == xter[j])
+			{
+				s[i] = val[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}

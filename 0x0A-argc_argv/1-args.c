c75b2dde57435a1ad5e02d4743c939e6dedcf917
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-25 10:07:50 -0700

    Adds Task 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argc[] __attribute__((unused)))
+/**
+ * main - prints the number of arguements passed
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }

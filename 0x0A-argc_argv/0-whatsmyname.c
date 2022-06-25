a71586a91964883ecb915c02bc9e562376e1084c
Cynthia Uche <cynthianuchepro@gmail.com>
2022-06-25 08:31:03 -0700

    Adds Script 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,13 @@
 
 /**
  * main - prints its name, followed by a new line.
- *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv[0]);
 	return (0);
 }

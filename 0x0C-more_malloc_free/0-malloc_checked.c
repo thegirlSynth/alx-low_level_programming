2036c3fff985ed47fd866f3d1777bd9844251e77
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-01 18:20:49 -0700

    Adds Script 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: the number of bytes to allocate.
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+		exit(98);
+
+	return (pointer);
+}

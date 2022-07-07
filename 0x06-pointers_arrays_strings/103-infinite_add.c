1ba5cffc0231a5b156c18f0e7153b421de8c83b8
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-07 04:23:24 -0700

    Adds Script 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,5 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-/**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @r_index: The current index of the buffer.
- *
- * Return: If r can store the sum - a pointer to the result.
- */
-
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-{
-	int num, tens = 0;
-
-	for (; *n1 && *n2; n1--, n2--, r_index--)
-	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	for (; *n1; n1--, r_index--)
-	{
-		num = (*n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	for (; *n2; n2--, r_index--)
-	{
-		num = (*n2 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	if (tens && r_index >= 0)
-	{
-		*(r + r_index) = (tens % 10) + '0';
-		return (r + r_index);
-	}
-
-	else if (tens && r_index < 0)
-		return (0);
-
-	return (r + r_index + 1);
-}
 
 /**
  * infinite_add - a function that adds two numbers.
@@ -63,21 +13,41 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int index, len1 = 0, len2 = 0;
+	int i = 0, j = 0, k = 0;
+	int sum = 0;
+	int tens = 0;
+	int begin = 0;
+	int swap = 0;
 
-	for (index = 0; *(n1 + index); index++)
-		len1++;
-
-	for (index = 0; *(n2 + index); index++)
-		len2++;
-
-	if (size_r <= len1 + 1 || size_r <= len2 + 1)
+	while (n1[i] != 0)/* A */
+		i++;
+	while (n2[j] != 0)
+		j++;
+	i--;/* C */
+	j--;
+	if (i > size_r || j > size_r)/* D */
 		return (0);
-
-	n1 += len1 - 1;
-	n2 += len2 - 2;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
+	for ( ; k < size_r; i--, j--, k++)/* E */
+	{
+		sum = tens;
+		if (i >= 0)/* F */
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+		if (i < 0 && j < 0 && sum == 0)/* G */
+			break;
+		tens = sum / 10;/* H */
+		r[k] = sum % 10 + '0';
+	}
+	if (i >= 0 || j >= 0 || sum > 0)/* J */
+		return (0);
+	r[k] = '\0';/* K */
+	k--;
+	for ( ; begin < k; k--, begin++)/* I */
+	{
+		swap = r[k];
+		r[k] = r[begin];
+		r[begin] = swap;
+	}
+	return (r);
 }
-

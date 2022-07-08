c53165bbd20b474efda7eca2590139d83b182518
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-08 16:50:30 -0700

    Adds header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 
-#endif // MAIN_H
+#endif // FUNCTION_POINTERS_H

2c3c3efdcceb92d16069f8487c08014a84f9bc98
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-07 19:14:29 -0700

    Adds Script 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list begin;
+	int i = 0, j = 0;
+	char *string;
+
+	va_start(begin, format);
+	while (format && format[i] != '\0')
+	{
+		switch (format[i])
+		{ case 'c':
+			switch (j)
+			{ case 1: printf(", "); }
+			j = 1;
+			printf("%c", va_arg(begin, int));
+			break;
+		case 'i':
+			switch (j)
+			{ case 1: printf(", "); }
+			j = 1;
+			printf("%i", va_arg(begin, int));
+			break;
+		case 'f':
+			switch (j)
+			{ case 1: printf(", "); }
+			j = 1;
+			printf("%f", va_arg(begin, int));
+			break;
+		case 's':
+			switch (j)
+			{ case 1: printf(", "); }
+			j = 1;
+			p = va_arg(list, char*);
+			if (p)
+			{ printf("%s", p);
+			break; }
+			printf("%p", p);
+			break; }
+		i++;
+	}
+	printf("\n");
+	va_end(begin);
+}

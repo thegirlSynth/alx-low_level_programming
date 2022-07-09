8defab2615fdfa2f4f499ea571f893340afcf3e7
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-08 18:05:17 -0700

    Adds Script 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: is the array of elements.
+ * @size: the number of elements in the array array
+ * @cmp:is a pointer to the function to be used to compare values
+ * Return: returns the index of the first element for which the cmp function
+ * does not return 0.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+

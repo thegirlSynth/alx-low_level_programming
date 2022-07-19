5062987687e3317a2c601e1b7a846e761001bfe1
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-18 18:54:54 -0700

    Adds file 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *firstNode;
 
 	endNode = malloc(sizeof(listint_t));
+	if (endNode == NULL)
+		return (NULL);
+
 	endNode->n = n;
 	endNode->next = NULL;
 

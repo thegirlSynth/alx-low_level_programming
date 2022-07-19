a08a147d8c6841bf33affabdf4d1b857abbeb5ee
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-18 17:20:18 -0700

    Adds File 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
 /**
  * listint_len -  returns the number of elements in a linked listint_t list.
  * @h: the list.
- * @Return: the number of elements in the list.
+ * Return: the number of elements in the list.
  */
 
 size_t listint_len(const listint_t *h)

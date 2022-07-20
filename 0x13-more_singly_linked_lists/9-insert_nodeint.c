5acfba85354ab510067d5e0a6b6c22e3d3832fa5
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-19 19:38:22 -0700

    Adds file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (newNode);
 		}
 		i++;
-		prevNode = prevNode->next;
+		else
+			prevNode = prevNode->next;
 	}
 	return (NULL);
 }

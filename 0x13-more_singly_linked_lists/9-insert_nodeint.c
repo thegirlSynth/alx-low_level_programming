3b34dfee7a479f91c8f19a09cb71cabcffe9900e
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-19 19:35:21 -0700

    Adds file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	prevNode = *head;
-	while (prevNode)
+	while (prevNode && i < idx)
 	{
 		if (i == (idx - 1))
 		{

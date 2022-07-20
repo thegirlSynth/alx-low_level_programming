09ca5f4d23ddb10e012487a5bab018fc162fcb64
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-19 17:35:04 -0700

    Adds File 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL || index < 0)
+	if (head == NULL)
 		return (NULL);
 
 	while (head)

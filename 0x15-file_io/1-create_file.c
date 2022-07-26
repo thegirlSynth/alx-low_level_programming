0809469ccf04418812f61d250ec2f22809296f0c
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-25 19:03:46 -0700

    Adds file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
+		close(filedes);
 		return (1);
+	}
 
 	while (text_content)
 		len++;

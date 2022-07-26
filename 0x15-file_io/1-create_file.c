a2ee3e15e3ade73ca10a99ff31f972a560994659
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-25 18:21:56 -0700

    Adds file 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int filedes, count, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	filedes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0400);
+
+	if (filedes == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	while (text_content)
+		len++;
+
+	count = write(filedes, text_content, len);
+	if (count == -1)
+		return (-1);
+
+	close(filedes);
+	return (1);
+}

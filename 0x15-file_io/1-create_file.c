8d3c567db9aa2782c45c5af75c72cc32b9169c0b
Cynthia Uche <cynthianuchepro@gmail.com>
2022-07-26 19:14:22 -0700

    Hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	while (text_content)
+	while (text_content[len])
 		len++;
 
 	count = write(filedes, text_content, len);

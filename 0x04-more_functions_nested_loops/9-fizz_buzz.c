f71b7fa4c1a067b01186415821cfc0e16c55c583
Cynthia Uche <cynthianuchepro@gmail.com>
2022-08-08 13:22:06 -0700

    Updates Script 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,17 +4,17 @@
 /**
  * main - prints the numbers from 1 to 100, followed by a new line.
  * But for multiples of three, it prints Fizz instead of the number
- * and for the multiples of five, it prints Buzz. 
+ * and for the multiples of five, it prints Buzz.
  * For numbers which are multiples of both three and five, it prints FizzBuzz.
  * Each number or word is separated by a space.
  * Return: 0 on success.
  */
 
-int main()
+int main(void)
 {
 	int index = 1;
 
-	printf("%d ", index);
+	printf("%d", index);
 
 	while (index > 0 && index <= 99)
 	{

1190b819604a3afb5f41f85a2c7073a4e8ff7c01
Cynthia Uche <cynthianuchepro@gmail.com>
2022-09-02 15:47:51 -0700

    Restructure code for better readability

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,31 +18,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->next = NULL;
-	newnode->prev = NULL;
+
+	if (idx == 0)
+	{
+		newnode->next = *h;
+		newnode->prev = NULL;
+		*h = newnode;
+		return (newnode);
+	}
+
 
 	while (checknode != NULL)
 	{
 		if (count == idx)
-			break;
+		{
+			newnode->prev = checknode->prev;
+			checknode->prev = newnode;
+			newnode->next = checknode;
+			newnode->prev->next = newnode;
+			return (newnode);
+		}
 		checknode = checknode->next;
 		count++;
 	}
 
-	if (idx >= count && checknode == NULL && *h != NULL)
-		return (NULL);
 
-	if (checknode != NULL)
-	{
-		newnode->prev = checknode->prev;
-		checknode->prev = newnode;
-		newnode->next = checknode;
-		if (count > 0)
-			newnode->prev->next = newnode;
-	}
 
-	if (count == 0)
-		*h = newnode;
-
-	return (newnode);
+	return (NULL);
 }

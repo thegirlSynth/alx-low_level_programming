cb8bc77b8991fba0c7d78c79e3a6a24bf261a5b7
Cynthia Uche <61497064+thegirlSynth@users.noreply.github.com>
2022-09-04 09:33:15 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		newnode->next = *h;
 		newnode->prev = NULL;
+		(*h)->prev = newnode;
 		*h = newnode;
 		return (newnode);
 	}

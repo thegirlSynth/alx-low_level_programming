abf7ecea3845c1078a73900bbaf02a3c0ed0ecf9
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-07-18 23:50:17 +0100

    Format header file and add putchar file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 
 /*Prototypes*/
 
@@ -12,6 +12,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
 
 #endif
